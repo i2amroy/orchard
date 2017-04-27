04b1fb8110fea2ccea714435f66882450b5f82a1
Christian Buskirk <i2amroy@gmail.com>
2017-04-27 01:19:19 -0700

    Finalize Branching 2

--- orchard/core/_branch.py
+++ orchard/core/_branch.py
@@ -8,7 +8,7 @@
 import copy
 import os
 from pathlib import Path
-from ..file import LogFile, LinkFile, ConfigFile
+from ..file import LogFile
 
 import yaml
 
@@ -290,8 +290,8 @@ def branching2(config_file, link_file, workspace_path):
     if not work_path.is_file():
         os.makedirs(os.path.join(workspace_path, "1"))
         logdata = LogFile(config_file, True)
-        branch_config_struct(config_file, "1")
         logdata.write(work_path)
+        branch_config_struct(config_file, link_file, "1")
         return config_file
 
     # If we made it this far then a branchlog file already exists, so read it
@@ -308,7 +308,7 @@ def branching2(config_file, link_file, workspace_path):
         # no need to mess around with the logfile since this branch already
         # exists in the logfile
         if perfect_branch:
-            branch_config_struct(config_file, branchnum)
+            branch_config_struct(config_file, link_file, branchnum)
             return config_file
 
         # Else see if the branch that we found is a better match then our
@@ -317,33 +317,43 @@ def branching2(config_file, link_file, workspace_path):
             branch_matching = matching_modules
             best_branchnum = branchnum
 
-        # Add the new branch to our logfile
-        new_branchnum = logs.add_branch(config_file)
-        # and make a new directory to hold it
-        os.makedirs(os.path.join(workspace_path, str(new_branchnum)))
-        # Then write out our updated logfile
-        logs.write(work_path)
+    # At this point we have our best branch figured out
+    # Add the new branch to our logfile
+    new_branchnum = logs.add_branch(config_file)
+    # and make a new directory to hold it
+    os.makedirs(os.path.join(workspace_path, str(new_branchnum)))
+    # Then write out our updated logfile
+    logs.write(work_path)
 
-        # For each dynamic path in our modules that match from a previous run
-        # we need to generate a symlink to the old dynamic path
-        for module in branch_matching:
-            for arg in module.get_dynamic_paths(
-                    link_file.get_module_data(module.name)):
-                path = arg.value
-                working_path = os.path.join(workspace_path, path)
-                oldpath = insert_branch_into_path(working_path, best_branchnum)
-                newpath = insert_branch_into_path(working_path, new_branchnum)
-                if not os.path.exists(newpath):
-                    os.symlink(
-                        os.path.relpath(oldpath, os.path.dirname(newpath)),
-                        newpath)
+    # For each dynamic path in our modules that match from a previous run
+    # we need to generate a symlink to the old dynamic path
+    for module in branch_matching:
+        for arg in module.get_dynamic_paths(
+                link_file.get_module_data(module.name)):
+            path = arg.value
+            working_path = os.path.join(workspace_path, path)
+            oldpath = insert_branch_into_path(working_path, best_branchnum)
+            newpath = insert_branch_into_path(working_path, new_branchnum)
+            print(path)
+            print(working_path)
+            print(oldpath)
+            print(newpath)
+            if not os.path.lexists(newpath):
+                os.symlink(
+                    os.path.relpath(oldpath, os.path.dirname(newpath)),
+                    newpath)
 
-        # Once we've generated all of the requisite symlinks we can just update
-        # the paths in our ConfigFile and we're done branching
-        branch_config_struct(config_file, new_branchnum)
-        return config_file
+    # Once we've generated all of the requisite symlinks we can just update
+    # the paths in our ConfigFile and we're done branching
+    branch_config_struct(config_file, link_file, new_branchnum)
+    return config_file
 
 
-def branch_config_struct(config_file, branchnum):
-    # TODO
-    pass
+def branch_config_struct(config_file, link_file, branchnum):
+    # Insert our branchnum into all of the dynamic paths of each module
+    for module in config_file.modules:
+        dyn_paths = module.get_dynamic_paths(
+            link_file.get_module_data(module.name))
+        for arg in dyn_paths:
+            if arg.value is not None:
+                arg.value = insert_branch_into_path(arg.value, branchnum)
